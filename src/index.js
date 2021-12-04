f9694d5458f8fa1ba269ae79a3e398b36b1a1a3f
Mathieu Quisefit <quisefit@wcentric.com>
2021-12-04 12:15:02 +0100

    Better implementation

--- src/index.js
+++ src/index.js
@@ -1,50 +1,61 @@
 const Program = require('commander')
 
-const command = Program.command
-Program.command = (...args) => {
-  return ConvictCommander(command.apply(Program, args))
+class ConvictedCommand extends Program.Command {
+	convict(convict) {
+		this._walkConvict(convict);
+		return this;
+	}
+
+	_walkConvict(convict, schema, path) {
+		schema || (schema = convict._def);
+		path || (path = []);
+		const keys = Object.keys(schema);
+		const self = this;
+		keys.forEach(function(paramName) {
+			const param = schema[paramName];
+			const subPath = [...path, paramName];
+
+			if (typeof param === 'object' && ( typeof param.doc === 'undefined' )) {
+				self._walkConvict(convict, param, subPath);
+				return;
+			}
+
+			if (!param.arg) {
+				return;
+			}
+
+			const def = '--' + param.arg + ( param.format !== Boolean ? ' <' + paramName + '>' : '' );
+
+			const opts = [];
+			if (param.default) {
+				opts.push('default: ' + param.default);
+			}
+			if (typeof param.env !== 'undefined') {
+				opts.push('env: ' + param.env);
+			}
+
+			const doc = param.doc + (param.default == null ? ' (mandatory)' : ' [' + opts.join(', ') + ']');
+
+			self.option(def, doc);
+		});
+	};
+
+	createCommand(name) {
+		return new ConvictedCommand(name);
+	};
 }
 
-function ConvictCommander(commander) {
-  function recurs(convict, schema, path) {
-    schema || (schema = convict._def)
-    path || (path = [])
-    const keys = Object.keys(schema)
-    keys.forEach(function(paramName) {
-      const param = schema[paramName];
-      const subPath = [...path, paramName]
+exports = module.exports = new ConvictedCommand();
+exports.program = exports;
 
-      if (typeof param === 'object' && ( typeof param.doc === 'undefined' )) {
-        recurs(convict, param, subPath)
-        return;
-      }
+/**
+ * Expose classes
+ */
 
-      if (!param.arg) {
-        return;
-      }
-
-      const def = '--' + param.arg + ( param.format !== Boolean ? ' <' + paramName + '>' : '' );
-
-      const opts = []
-      if (param.default) {
-        opts.push('default: ' + param.default)
-      }
-      if (typeof param.env !== 'undefined') {
-        opts.push('env: ' + param.env)
-      }
-
-      const doc = param.doc + (param.default == null ? ' (mandatory)' : ' [' + opts.join(', ') + ']');
-
-      commander.option(def, doc);
-    });
-  };
-
-  commander.convict = function conv(convict) {
-    recurs(convict);
-    return this;
-  };
-
-  return commander;
-};
-
-module.exports = ConvictCommander(Program)
+exports.Argument = Program.Argument;
+exports.Command = ConvictedCommand;
+exports.CommanderError = Program.CommanderError;
+exports.Help = Program.Help;
+exports.InvalidArgumentError = Program.InvalidArgumentError;
+exports.InvalidOptionArgumentError = Program.InvalidArgumentError; // Deprecated
+exports.Option = Program.Option;
